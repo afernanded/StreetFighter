6e7d6dfcfdde8319a7c2d9c2201e5e53de959fa2
Alexander <81477736+afernanded@users.noreply.github.com>
2021-04-15 17:53:39 +0200

    [FIX] Minor import fixes

--- StreetFighter/src/app/app.module.ts
+++ StreetFighter/src/app/app.module.ts
@@ -3,17 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { RetratoLuchadorComponent } from './components/retrato-luchador/retrato-luchador.component';
-import {AreaSeleccionComponent} from "./components/area-seleccion/area-seleccion.component";
+import {AreaSeleccionComponent} from './components/area-seleccion/area-seleccion.component';
 import { AtributosLuchadorComponent } from './components/atributos-luchador/atributos-luchador.component';
 import { BienvenidaComponent } from './components/bienvenida/bienvenida.component';
-import {RouterModule} from "@angular/router";
+import {RouterModule} from '@angular/router';
 import { AntesLucharComponent } from './components/antes-luchar/antes-luchar.component';
-import {DetallesProductoGuardaService} from "../../../proyecto-productos/src/app/servicios/detalles-producto-guarda.service";
-import {LuchadorResolveService} from "./servicios/luchador-resolve.service";
-import {CargaProductoService} from "../../../proyecto-productos/src/app/servicios/carga-producto.service";
-import {HttpClientModule} from "@angular/common/http";
+import {LuchadorResolveService} from './servicios/luchador-resolve.service';
+import {HttpClientModule} from '@angular/common/http';
 import { ModificarRetratosComponent } from './components/modificar-retratos/modificar-retratos.component';
-import {EditRetratoComponent} from "./components/edit-retrato/edit-retrato.component";
+import {EditRetratoComponent} from './components/edit-retrato/edit-retrato.component';
 
 @NgModule({
   declarations: [
@@ -46,10 +44,11 @@ import {EditRetratoComponent} from "./components/edit-retrato/edit-retrato.compo
           path: 'modificar-retratos', component: ModificarRetratosComponent
         },
         {path: '', redirectTo: '/bienvenida', pathMatch: 'full'},
-        {path: '**', redirectTo: '/bienvenida', pathMatch: 'full'},]
+        {path: '**', redirectTo: '/bienvenida', pathMatch: 'full'}, ]
     )
   ],
-  providers: [CargaProductoService, Title, DetallesProductoGuardaService],
+  providers: [Title],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
